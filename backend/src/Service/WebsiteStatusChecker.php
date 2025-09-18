50f4d7d9736fa1a023ada8af9f0df962595fceb0
Hasan-Alherek <hasanalherek95@gmail.com>
2025-09-18 15:07:40 +0000

    Add WebsiteStatusChecker.php service and WebsiteStatusLogManager.php manager

---
+++ backend/src/Service/WebsiteStatusChecker.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\WebsiteStatusLog;
+use App\Manager\WebsiteStatusLogManager;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class WebsiteStatusChecker
+{
+    public function __construct
+    (
+        private HttpClientInterface $httpClient,
+        private WebsiteStatusLogManager $websiteStatusLogManager,
+    ) {}
+
+    public function check($website):  WebsiteStatusLog
+    {
+        $responseTimeStart = microtime(true);
+        $response = $this->httpClient->request(
+            'GET',
+            $website->getUrl(),
+            [
+                'timeout' => 5
+            ]
+        );
+        $responseTimeEnd = microtime(true);
+        $responseTime = ($responseTimeEnd - $responseTimeStart) * 1000;
+        $statusCode = $response->getStatusCode() ?? 0;
+        $checkedAt = new \DateTimeImmutable();
+        $websiteId= $website->getWebsiteId();
+
+         return $this->websiteStatusLogManager->add(
+            $websiteId,
+            $statusCode,
+            $responseTime,
+            $checkedAt
+        );
+    }
+
+}
